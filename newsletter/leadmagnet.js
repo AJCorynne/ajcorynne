9a0bd2d5f569b61a06f6fc717eb6951f03300e50
AJCorynne <hello@ajcorynne.com>
2026-09-06 15:20:19 -0600

    Update leadmagnet.js

--- newsletter/leadmagnet.js
+++ newsletter/leadmagnet.js
@@ -5,8 +5,8 @@
 
 const LEAD_MAGNET_CONFIG = {
   badge: "Complimentary Sci-Fi Romance Novella",
-  titlePrefix: "Claim Your Free Copy of ",
-  bookTitle: "Taliah’s Poem",
+  titlePrefix: "Claim Your ",
+  bookTitle: "Free Copy",
   blurb: "In GOD OF SKIES, Taliah's perspective is kept mysterious, but in her novella, TALIAH'S POEM, which runs in a parallel timeline to book 1, she exposes all her secrets. <br />After her closest confidant vanishes, Taliah is left stranded on a planet where the political walls are closing in, forcing her simultaneously into a secret alliance and a dangerous attraction, both of which could turn on her fast.",
   coverImage: "/images/Skyreacher-1.5-Taliah's-Poem-book-cover.jpg",
   formId: "5e996bde-a4c1-11f1-8de1-71ba425265d3"
